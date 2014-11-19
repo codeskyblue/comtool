b0f62fa2a373b2c249ed340ef00f97e02f2d46cb
codeskyblue <codeskyblue@gmail.com>
2014-11-19 17:02:06 +0800

    add some funcs

--- file.go
+++ file.go
@@ -1,8 +1,10 @@
 package comtool
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 // Return program dir
@@ -18,4 +20,33 @@ func Exists(name string) bool {
 		}
 	}
 	return true
+}
+
+func IsDir(name string) bool {
+	info, err := os.Stat(name)
+	return err == nil && info.IsDir()
+}
+
+func IsFile(name string) bool {
+	info, err := os.Stat(name)
+	return err == nil && !info.IsDir()
+}
+
+// HomeDir returns path of '~'(in Linux) on Windows,
+// it returns error when the variable does not exist.
+func HomeDir() (home string, err error) {
+	if runtime.GOOS == "windows" {
+		home = os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+	} else {
+		home = os.Getenv("HOME")
+	}
+
+	if len(home) == 0 {
+		return "", errors.New("Cannot specify home directory because it's empty")
+	}
+
+	return home, nil
 }
